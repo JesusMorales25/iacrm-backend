af796ec5db71dbd43c957285db489a3546ed2d1e
JesusMorales25 <morsar25@gmail.com>
2025-08-21 03:40:44 -0500

    feature2

--- src/main/java/com/chatgpt/backend/config/SecurityConfig.java
+++ src/main/java/com/chatgpt/backend/config/SecurityConfig.java
@@ -55,8 +55,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:8080",
-                "https://assistant-iacrm-builderbot-production.up.railway.app"
+                "https://assistant-iacrm-builderbot-production.up.railway.app",
+								"https://iacrmapp.vercel.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
